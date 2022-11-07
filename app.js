06a3588bc3c5456aae3efe5ea34ea0b8b6e91a6c
Henry Huntington <henryxhuntington@gmail.com>
2022-11-07 14:24:04 -0800

    added eventListener: friend button

--- app.js
+++ app.js
@@ -44,25 +44,31 @@ addFriendButton.addEventListener('click', () => {
     // get the name from the input
     let newFriends = friendInputEl.value;
     // create a new friend object
-    const newFriend = 
+    const newFriend = {
+        name: newFriends,
+        satisfaction: 1,
+    };
     // push it into the friends state array, passed in as an argument
+    friendData.push(newFriend);
     // reset the input
+    friendInputEl.value = '';
     // display all the friends (use a function here)
+    displayFriends();
 });
 
 function displayFriends() {
     // clear out the friends in DOM
-
+    friendsEl.textContent = '';
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
-
+        const friendEl = renderFriend(friend);
         // this is a clickable list, so . . .
+
         //     add an event listener to each friend
         //         and if the friend's satisfaction level is below 3 and you have mushrooms left
         //             increment the friends satisfaction and decrement your mushrooms
         //             then display your friends and mushrooms with the updated state
-
         // append the friendEl to the friends list in DOM
     }
 }
